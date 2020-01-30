ae8124276409a828cdad12cc2a689ac2f872626a
Your Name <you@example.com>
2020-01-30 23:32:28 +0200

    second commit

--- js/main .js
+++ js/main .js
@@ -5,17 +5,15 @@ class Count {
   }
 } //end of the class
 
-let down = setInterval(function() {
-  dateLogic();
-}, 1000);
 //select button and put event on it
 var btn = document.getElementById("btn");
-btn.addEventListener("click", function dateLogic(e) {
+btn.addEventListener("click", function(e) {
   e.preventDefault();
   // convert eventDate to milliseconds
-  let eventDate = document.getElementById("date").getTime();
+  let eventDate = new Date(document.getElementById("date").value);
+  eventDate.getTime();
   //convert today's date to milliseconds
-  const nowDate = newDate().getTime();
+  const nowDate = new Date().getTime();
   //milliseconds premium
   let premium = eventDate - nowDate;
   //convert from milliseconds to days,hours,minutes and seconds
@@ -24,25 +22,34 @@ btn.addEventListener("click", function dateLogic(e) {
     (premium % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
   const minutes = Math.floor((premium % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((premium % (1000 * 60)) / 1000);
+  let seconds = Math.floor((premium % (1000 * 60)) / 1000);
+  let down = setInterval(function() {
+    eventDate = eventDate - 1; //decrease the count
+  }, 1000);
   //check of milliseconds
-  if (premium > 0) {
-    premium = premium - 1; //decrease the count
-  }
+
   //the count finshed.
-  else {
-    down.innerHtml("The Even is Now!");
-  }
+  // else {
+  //   down.innerHtml("The Even is Now!");
+  // }
+
   //new object from class count
-  countObj = new Count(
-    document.getElementById("title"),
-    document.getElementById("date")
+  let countObj = new Count(
+    document.getElementById("title".value),
+    document.getElementById("date".value)
   );
   //select event div and append text to it .
   const mainEvent = document.getElementById("event");
   const text = document.createElement("h2");
-  text.innerHTML(
-    days + "Dayes" + hours + "Hours" + minutes + "Minutes" + seconds + "Seconds"
-  );
+  text.innerHTML =
+    days +
+    "Dayes  " +
+    hours +
+    "Hours  " +
+    minutes +
+    "Minutes  " +
+    seconds +
+    "Seconds  ";
+
   mainEvent.appendChild(text);
 });
